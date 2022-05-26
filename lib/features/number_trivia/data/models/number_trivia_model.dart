122c0fb7ca10583d1a1a58131ed4360f247444b5
mustafiz012 <mustafiz012@outlook.com>
2022-05-26 12:10:55 +0600

    Writing Model class with data modification in Data Layer

---
+++ lib/features/number_trivia/data/models/number_trivia_model.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_tdd_clean_practice/features/number_trivia/domain/entities/number_trivia.dart';
+
+class NumberTriviaModel extends NumberTrivia {
+  const NumberTriviaModel({required String text, required int number})
+      : super(text, number);
+
+  factory NumberTriviaModel.fromJson(Map<String, dynamic> json) {
+    return NumberTriviaModel(
+        text: json['text'], number: (json['number'] as num).toInt());
+  }
+
+  Map<String, dynamic> toJson() {
+    return {'text': text, 'number': number};
+  }
+}
